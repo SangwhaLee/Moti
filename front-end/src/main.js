b04b99809e42fea38579db5051ba5825da1365c8
고명진 <mgin0527@naver.com>
2023-02-08 14:21:01 +0900

    카카오 로그인 수정

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -24,18 +24,12 @@ Vue.prototype.$axios = axios
 store.$axios = axios
 
 // const baseUrl = 'http://i8a509.p.ssafy.io:8080'
-//const baseUrl = 'https://moti.today/api'
-const baseUrl = 'http://localhost:8080/api'
+const baseUrl = 'https://moti.today/api'
+//const baseUrl = 'http://localhost:8080/api'
 Vue.prototype.$baseUrl = baseUrl
 store.$baseUrl = baseUrl
 window.Kakao.init("332addc510b9cc23dc54269380c84107");
 
-// const gauthOption = {
-//   clientId: '293618779849-da4im7oh35dq851ro2l9htrvi4ivbtc0.apps.googleusercontent.com',
-//   scope: 'profile email',
-//   prompt: 'select_account'
-// }
-// Vue.use(GAuth, gauthOption)
 // store에서 router 사용하기
 store.$router = router
 
